8a8b8e6ef59d7759470ce3889642c510f96eeecd
Alexandr <shefera0103@gmail.com>
2023-10-04 08:43:14 +0700

    add mvc resource config

--- src/main/java/ru/shefer/ApplicationConfig.java
+++ src/main/java/ru/shefer/ApplicationConfig.java
@@ -2,9 +2,15 @@ package ru.shefer;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("ru.shefer")
-public class ApplicationConfig  {
-
+public class ApplicationConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/css/");
+    }
 }
